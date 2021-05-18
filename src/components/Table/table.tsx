d30b6ed4de2d12b8f4e98e68d33022491cc976f6
zhou teng <user@administratordeMacBook-Pro-4.local>
2021-05-18 11:42:29 +0800

    add button component

---
+++ src/components/Table/table.tsx
@@ -0,0 +1,8 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
+import classNames from "classnames";
+import './style.scss'
+export const Button: FC = (props) => {
+  return <div className="btn">123</div>;
+};
+
+export default Button;
